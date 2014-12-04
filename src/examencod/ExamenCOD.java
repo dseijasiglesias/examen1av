448e9503c1a3c72bae126d402533140a46a7878c
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2014-12-04 09:51:02 +0100

    commit con comentario del segudno apartado realizado

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -1,4 +1,3 @@
-
 package examencod;
 
 public class ExamenCOD {
@@ -7,26 +6,32 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- int iN = 11;
-            	if (metodoMio(iN))
-System.out.println("Es numero primo");
-else
- System.out.println("NO es numero primo");           	 
-          	 
-for (int x=2;x<1000;x++) {
-if (metodoMio(x))
-System.out.print(x + " ");                         	 
-}         	 
-	}
-public static boolean metodoMio(int variable){
-int j = 2;
-boolean aux=true;
-while ((aux) && (j!=variable)){
-if (variable % j == 0)
-aux = false;
-j++;
-}
-return aux;
-  } 	 
+        int iN = 22;
+        if (metodoMio(iN)) {
+            System.out.println("Es numero primo");
+        } else {
+            System.out.println("NO es numero primo");
+        }
+
+        for (int x = 2; x < 1000; x++) {
+            if (metodoMio(x)) {
+                System.out.print(x + " ");
+            }
+        }
+    }
+
+    //cuando ((aux) && (j != variable)) tiene valor j=21, y variable=22 el valor es true 
     
+    public static boolean metodoMio(int variable) {
+        int j = 21;
+        boolean aux = true;
+        while ((aux) && (j != variable)) {
+            if (variable % j == 0) {
+                aux = false;
+            }
+            j++;
+        }
+        return aux;
+    }
+
 }
